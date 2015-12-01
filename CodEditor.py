4a453bc228627ebd330ded56e1bdc809e5ec37a1
PowerCod <power.cod96@gmail.com>
2015-12-01 15:48:50 +0800

    change from 'button' to 'Menu'

--- CodEditor.py
+++ CodEditor.py
@@ -1,15 +1,50 @@
 #! /usr/bin/env python
 #coding=utf-8
 
-import Tkinter
+from Tkinter import *
 
-top = Tkinter.Tk()
+
+def donothing():
+    filewin = Toplevel(top)
+    button = Button(filewin, text="Do nothing button")
+    button.pack()
+    
+def create_menu(root):
+    menubar = Menu(root)
+
+    filemenu = Menu(menubar, tearoff=0)
+    filemenu.add_command(label="新建", command=donothing)
+    filemenu.add_command(label="打开", command=donothing)
+    filemenu.add_command(label="保存", command=donothing)
+    filemenu.add_command(label="另存为", command=donothing)
+    filemenu.add_separator()
+    filemenu.add_command(label="退出", command=donothing)
+    menubar.add_cascade(label="文件", menu=filemenu)
+    
+    editmenu = Menu(menubar, tearoff=0)
+    editmenu.add_command(label="撤销")
+    editmenu.add_command(label="恢复")
+    
+    editmenu.add_separator()
+    editmenu.add_command(label="剪切")
+    editmenu.add_command(label="复制")
+    editmenu.add_command(label="粘贴")
+    editmenu.add_command(label="删除")
+    menubar.add_cascade(label="编辑", menu=editmenu)
+    
+    helpmenu = Menu(menubar, tearoff=0)
+    helpmenu.add_command(label="关于", command=donothing)
+    menubar.add_cascade(label="帮助", menu=helpmenu)
+
+    return menubar
+
+top = Tk()
 top.geometry('600x400+100+100')
 
-File_Button = Tkinter.Button(top, text="File",justify='left')
-Edit_Button = Tkinter.Button(top, text="Edit")
 
-File_Button.pack()
-Edit_Button.pack(after=File_Button)
+
+top.config(menu=create_menu(top))
+
+
 
 top.mainloop()
