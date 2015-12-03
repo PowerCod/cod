bd0550f3d3817d276b7d4ef19bee569b9a11c122
PowerCod <power.cod96@gmail.com>
2015-12-03 17:28:50 +0800

    add function open

--- CodEditor.py
+++ CodEditor.py
@@ -2,10 +2,23 @@
 #coding=utf-8
 
 from Tkinter import *
+import tkMessageBox
+from FileDialog import *
+
 
 WIDTH=600
 HEIGHT=400
+filename = ""
 
+def open_file():
+    global text_Window
+    fd = LoadFileDialog(root)
+    filename=fd.go()
+    f  = open(""+filename,'r')
+    content = f.read()
+    length = len(content)
+    text_Window.delete('1.0', '99999999.0')
+    text_Window.insert(END, content)
 
 def donothing():
     pass
@@ -14,44 +27,58 @@ def donothing():
     button.pack()'''
     
 def show_help():
-    pass
+    tkMessageBox.showinfo("about", "Cod Editor demo")
     
 def create_menu(root):
     menubar = Menu(root)
 
+    right_menu = Menu(root)
+
     filemenu = Menu(menubar, tearoff=0)
-    for item in ['新建','打开','保存','另存为']:
-        filemenu.add_command(label=item, command=donothing)
+    filemenu.add_command(label='新建', command=donothing)
+    filemenu.add_command(label='打开', command=open_file)
+    filemenu.add_command(label='保存', command=donothing)
+    filemenu.add_command(label='另存为', command=donothing)    
     filemenu.add_separator()
     filemenu.add_command(label="退出", command=donothing)
-    menubar.add_cascade(label="文件", menu=filemenu)
+    
     
     editmenu = Menu(menubar, tearoff=0)
     for item in ['撤销','恢复']:
         editmenu.add_command(label=item)
-    
     editmenu.add_separator()
     for item in ['剪切','复制','粘贴','删除']:
         editmenu.add_command(label=item)
-    menubar.add_cascade(label="编辑", menu=editmenu)
+   
     
     helpmenu = Menu(menubar, tearoff=0)
     helpmenu.add_command(label="关于", command=show_help)
+
+    
+    menubar.add_cascade(label="文件", menu=filemenu)
+    menubar.add_cascade(label="编辑", menu=editmenu)
     menubar.add_cascade(label="帮助", menu=helpmenu)
 
+
+    for x in ['复制', '剪切', '粘贴']:
+        right_menu.add_command(label=x, command=show_help)
+    def pop(event):
+        right_menu.post(event.x_root, event.y_root)
+    root.bind("<Button-3>", pop)
+        
     return menubar
 
 
-def main_window():
-    root = Tk()
-    root.wm_title("CodEditor demo")
-    root.geometry('600x400+100+100')
+
+root = Tk()
+root.wm_title("CodEditor demo")
+root.geometry('600x400+100+100')
     
-    e=Entry(root, width=WIDTH)
-    e.pack()
-    root.config(menu=create_menu(root))
-    root.mainloop()
-    
-    
-if __name__ == '__main__':
-    main_window()
+text_Window=Text(root)
+text_Window.pack(expand=YES, fill=BOTH)
+scroll = Scrollbar(text_Window)
+text_Window.config(yscrollcommand=scroll.set)
+scroll.config(command=text_Window.yview)
+scroll.pack(side=RIGHT, fill=Y)
+root.config(menu=create_menu(root))
+root.mainloop()
