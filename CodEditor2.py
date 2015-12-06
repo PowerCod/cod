ec158dd892fcf6d7586bfa60df58fba0a88ba594
BuildTools <unconfigured@null.spigotmc.org>
2015-12-06 22:59:38 +0800

    add menu

--- CodEditor2.py
+++ CodEditor2.py
@@ -7,11 +7,34 @@ a wxPython Editor
 import wx
 
 class Frame(wx.Frame):
-    pass
+    def __init__(self, parent, id, title):
+        wx.Frame.__init__(self, parent, id, title)
+        panel = wx.Panel(self)
+        statusBar = self.CreateStatusBar()
+        menuBar = wx.MenuBar()
+        menu_File = wx.Menu()
+        menu_File.Append(wx.NewId(), "&New")
+        menu_File.Append(wx.NewId(), "&Open")
+        menu_File.Append(wx.NewId(), "&Save")
+        menu_File.AppendSeparator()
+        menu_File.Append(wx.NewId(), "&Close")        
+        menu_File.AppendSeparator()
+        menu_File.Append(wx.NewId(), "Quit")
+        
+        menuBar.Append(menu_File, "&File")
+        
+        
+        menu_Edit = wx.Menu()
+        menu_Edit.Append(wx.NewId(), "&Cut")
+        menu_Edit.Append(wx.NewId(), "&Copy")
+        menu_Edit.Append(wx.NewId(), "&Paste")
+        menuBar.Append(menu_Edit, "&Edit")
+        self.SetMenuBar(menuBar)
+        
 
 class App(wx.App):
     def OnInit(self):
-        self.frame = Frame(None, title="hehe")
+        self.frame = Frame(None, id=-1, title="CodEditor2 Demo")
         self.frame.Show()        
         return True
     
